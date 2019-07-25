5fa99b0165f24f32592e009afae7115091fb65b5
elodieOC <elodie.ollivier.pro@gmail.com>
2019-07-25 09:13:20 +0200

    first commit

---
+++ microservice-clientui/src/main/java/com/mclientui/microserviceclientui/MicroserviceClientuiApplication.java
@@ -0,0 +1,13 @@
+package com.mclientui.microserviceclientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceClientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceClientuiApplication.class, args);
+	}
+
+}
