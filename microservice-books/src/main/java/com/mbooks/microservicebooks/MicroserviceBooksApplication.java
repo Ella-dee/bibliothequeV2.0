c42c504c34b44ca018ff72d04b6ae0ccfef0386b
elodieOC <elodie.ollivier.pro@gmail.com>
2019-08-02 15:18:44 +0200

    liaison des microservices à eureka

--- microservice-books/src/main/java/com/mbooks/microservicebooks/MicroserviceBooksApplication.java
+++ microservice-books/src/main/java/com/mbooks/microservicebooks/MicroserviceBooksApplication.java
@@ -2,8 +2,10 @@ package com.mbooks.microservicebooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceBooksApplication {
 
 	public static void main(String[] args) {
