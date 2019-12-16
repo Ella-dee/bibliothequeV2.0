deae925954e54cb0f3b059b756ad47fec066a71d
elodieOc <elodie.ollivier.pro@gmail.com>
2019-12-16 11:08:23 +0100

    Issue 2: -Corrigez un bug dans la gestion des prolongations de prêt (par le client): un prêt en retard ne peux plus être prolongé. les batch mails ne sont plus envoyé pour renouvellement si le pret est en retard.

--- microservice-mailing/src/main/java/com/mmailing/microservicemailing/batch/ScheduledTasks.java
+++ microservice-mailing/src/main/java/com/mmailing/microservicemailing/batch/ScheduledTasks.java
@@ -21,6 +21,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
+import static com.mmailing.microservicemailing.utils.DateUtils.*;
+
 /**
  * <h1>Class that checks late status for borrowings and send mails accordingly</h1>
  */
@@ -39,34 +41,36 @@ public class ScheduledTasks {
 
     /**
      * <p>method called when application is ran</p>
-     *  <p>checks if borrowing status is "late" and renewable, and if so sends mails</p>
+     *  <p>checks if borrowing comes to an end (1 week) and renewable, and if so sends mails</p>
      */
     @Scheduled(cron="0 0 18 * * MON-FRI") //Fire at 6pm every weekday
     public void checkAndSendRenewableBorrowings(){
         List<BorrowingBean> borrowings = booksProxy.listBorrowings();
         for(BorrowingBean borrowing:borrowings) {
             //get info and calls proper mailMessage for borrowings that can be renewed
-            if (borrowing.getBorrowingType().getId() == 4 && borrowing.getRenewed() == false) {
-                    UserBean user = usersProxy.showUser(borrowing.getIdUser());
-                    BookBean book = booksProxy.showBook(borrowing.getBook().getId());
-                    String subject = "Retour du livre " + book.getTitle();
-                    String text = "Bonjour "+user.getFirstName()+" "+user.getLastName()+
-                            "\n\nVous avez emprunté le livre '"+book.getTitle()+"' le "+ borrowing.getBorrowed()+"."+
-                            "\nLa date limite de retour était le "+borrowing.getLimitDate()+"."+
-                            "\nVous avez la possibilité de renouveler une fois votre emprunt, pour une durée de 4semaines."+
-                            "\nMerci de bien vouloir vous rendre à la bibliothèque, ou répondre à ce mail, afin d'effectuer "+
-                            "le retour du livre ou la prolongation de l'emprunt."+
-                            "\n\nA bientôt et bonne lecture, "+
-                            "\n\nLa bilibothèque de la ville";
+            Date date = convertStringToDateFormat(borrowing.getLimitDate());
+            LocalDate limitDate = convertToLocalDateViaInstant(date);
+            if (theDateToday().minusDays(7)==limitDate && borrowing.getBorrowingType().getId() == 1 && borrowing.getRenewed() == false) {
+                UserBean user = usersProxy.showUser(borrowing.getIdUser());
+                BookBean book = booksProxy.showBook(borrowing.getBook().getId());
+                String subject = "Retour du livre " + book.getTitle();
+                String text = "Bonjour " + user.getFirstName() + " " + user.getLastName() +
+                        "\n\nVous avez emprunté le livre '" + book.getTitle() + "' le " + borrowing.getBorrowed() + "." +
+                        "\nLa date limite de retour était le " + borrowing.getLimitDate() + "." +
+                        "\nVous avez la possibilité de renouveler une fois votre emprunt, pour une durée de 4semaines." +
+                        "\nMerci de bien vouloir vous rendre à la bibliothèque, ou répondre à ce mail, afin d'effectuer " +
+                        "le retour du livre ou la prolongation de l'emprunt." +
+                        "\n\nA bientôt et bonne lecture, " +
+                        "\n\nLa bilibothèque de la ville";
 
-                    try{
-                        mailService.sendSimpleMessage(user.getEmail(), subject, text);
-                        MailSent newMailSent = prepareModelMailSent(localDateToString(theDateToday()),  user.getId(), book.getId(), borrowing.getId());
-                        newMailSent.setMailType(mailTypeDao.findMailTypeById(2));
-                        mailSentDao.save(newMailSent);
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
+                try {
+                    mailService.sendSimpleMessage(user.getEmail(), subject, text);
+                    MailSent newMailSent = prepareModelMailSent(localDateToString(theDateToday()), user.getId(), book.getId(), borrowing.getId());
+                    newMailSent.setMailType(mailTypeDao.findMailTypeById(2));
+                    mailSentDao.save(newMailSent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             else {
                 System.out.println("NO BORROWINGS THAT COULD BE RENEWED");
@@ -86,25 +90,25 @@ public class ScheduledTasks {
         for(BorrowingBean borrowing:borrowings) {
             //get each late borrowing that cannot be renewed
             if (borrowing.getBorrowingType().getId() == 4 && borrowing.getRenewed() == true) {
-                    UserBean user = usersProxy.showUser(borrowing.getIdUser());
-                    BookBean book = booksProxy.showBook(borrowing.getBook().getId());
-                    String subject = "Retour du livre " + book.getTitle();
-                    String text = "Bonjour " + user.getFirstName() + " " + user.getLastName() +
-                            "\n\nVous avez emprunté le livre '" + book.getTitle() + "' le " + borrowing.getBorrowed() + "." +
-                            "\nLa date limite de retour était le " + borrowing.getLimitDate() + "." +
-                            "\nVous avez déjà renouvelé votre emprunt une fois." +
-                            "\nMerci de bien vouloir vous rendre à la bibliothèque, " +
-                            "et d'effectuer le retour du livre." +
-                            "\n\nA bientôt et bonne lecture, " +
-                            "\n\nLa bilibothèque de la ville";
-                    try {
-                        mailService.sendSimpleMessage(user.getEmail(), subject, text);
-                        MailSent newMailSent = prepareModelMailSent(localDateToString(theDateToday()), user.getId(), book.getId(), borrowing.getId());
-                        newMailSent.setMailType(mailTypeDao.findMailTypeById(1));
-                        mailSentDao.save(newMailSent);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                UserBean user = usersProxy.showUser(borrowing.getIdUser());
+                BookBean book = booksProxy.showBook(borrowing.getBook().getId());
+                String subject = "Retour du livre " + book.getTitle();
+                String text = "Bonjour " + user.getFirstName() + " " + user.getLastName() +
+                        "\n\nVous avez emprunté le livre '" + book.getTitle() + "' le " + borrowing.getBorrowed() + "." +
+                        "\nLa date limite de retour était le " + borrowing.getLimitDate() + "." +
+                        "\nVous avez déjà renouvelé votre emprunt une fois." +
+                        "\nMerci de bien vouloir vous rendre à la bibliothèque, " +
+                        "et d'effectuer le retour du livre." +
+                        "\n\nA bientôt et bonne lecture, " +
+                        "\n\nLa bilibothèque de la ville";
+                try {
+                    mailService.sendSimpleMessage(user.getEmail(), subject, text);
+                    MailSent newMailSent = prepareModelMailSent(localDateToString(theDateToday()), user.getId(), book.getId(), borrowing.getId());
+                    newMailSent.setMailType(mailTypeDao.findMailTypeById(1));
+                    mailSentDao.save(newMailSent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             else {
                 System.out.println("NO DEFO LATE BORROWINGS");
@@ -129,21 +133,4 @@ public class ScheduledTasks {
         return model;
     }
 
-    /**
-     * <p>Gets LocalDate for today</p>
-     * @return localDate
-     */
-    public LocalDate theDateToday(){
-        ZoneId zone = ZoneId.of("Europe/Paris");
-        LocalDate today = LocalDate.now(zone);
-        return today;
-    }
-    /**
-     * <p>Parses a LocalDate to string</p>
-     * @return string
-     */
-    public String localDateToString(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return date.format(formatter);
-    }
 }
